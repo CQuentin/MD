6745b0d357d45028e9931513eaa3e0dc73a06f28
dEsk <desk@desk-CX61-2PC.(none)>
2014-12-17 13:45:38 +0100

    Ajout tests Grid

--- code/CandyCrush/src/model/test/GridTest.java
+++ code/CandyCrush/src/model/test/GridTest.java
@@ -1,6 +1,8 @@
 package model.test;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Circle;
 import model.ContentCase;
@@ -13,28 +15,35 @@ import org.junit.Test;
 
 public class GridTest {
 	public Grid grid;
-	Circle cBlack;
-	Circle cWhite;
+	private Circle cBlack;
+	private Circle cWhite;
+	private EmptyCase emptyCase;
+	private List<ContentCase> listOfContents;
 	
 	public GridTest() {
-		grid = new Grid(2,1);
+		grid = new Grid(1,2);
 		cBlack= new Circle(Color.BLACK);
 		cWhite = new Circle(Color.WHITE);
+		emptyCase = new EmptyCase();
+		listOfContents = new ArrayList<ContentCase>();
+		listOfContents.add(cBlack);
+		listOfContents.add(cWhite);
+		grid.setList(listOfContents);
 	}
 	
 	@Before
 	public void startUp(){
 		grid.getContentGrid()[0][0] = cBlack;
-		grid.getContentGrid()[1][0] = cWhite;
+		grid.getContentGrid()[0][1] = cWhite;
 	}
 	
 	@Test
 	public void testSwap() {
-		grid.swap(0, 0, 1, 0);
+		grid.swap(0, 0, 0, 1);
 		
-		ContentCase[][] expected = new ContentCase[2][1];
+		ContentCase[][] expected = new ContentCase[1][2];
 		expected[0][0] = cWhite;
-		expected[1][0] = cBlack;
+		expected[0][1] = cBlack;
 		
 		Assert.assertArrayEquals(grid.getContentGrid(),expected);
 	}
@@ -47,12 +56,53 @@ public class GridTest {
 	
 	@Test
 	public void testCasesEqual1(){
-		Assert.assertFalse(grid.casesEqual(grid.getContentGrid()[0][0],grid.getContentGrid()[1][0]));
+		Assert.assertFalse(grid.casesEqual(grid.getContentGrid()[0][0],grid.getContentGrid()[0][1]));
 	}
 	
 	@Test
 	public void testCasesEqual2(){
-		grid.getContentGrid()[1][0] = cBlack;
-		Assert.assertTrue(grid.casesEqual(grid.getContentGrid()[0][0],grid.getContentGrid()[1][0]));
+		grid.getContentGrid()[0][1] = cBlack;
+		Assert.assertTrue(grid.casesEqual(grid.getContentGrid()[0][0],grid.getContentGrid()[0][1]));
 	}
+	
+	@Test
+	public void testFill1() {
+		Assert.assertFalse(grid.fill());
+	}
+	
+	/* Test non modification dans le cas d'une grille pleine */
+	@Test
+	public void testFill2() {
+		ContentCase[][] expected = grid.getContentGrid();
+		grid.fill();
+		ContentCase[][] actual = grid.getContentGrid();
+		
+		Assert.assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void testFill3() {
+		grid.getContentGrid()[0][0] = emptyCase.clone();
+		Assert.assertTrue(grid.fill());
+	}
+	
+	@Test
+	public void testFill4() {
+		grid.getContentGrid()[0][0] = emptyCase.clone();
+		grid.fill();
+		ContentCase[][] actual = grid.getContentGrid();
+		
+		Assert.assertFalse(actual[0][0] instanceof EmptyCase);
+	}
+	
+	@Test
+	public void testFill5() {
+		grid.getContentGrid()[0][1] = emptyCase.clone();
+		ContentCase expected = grid.getContentGrid()[0][0];
+		grid.fill();
+		ContentCase actual = grid.getContentGrid()[0][1];
+		
+		Assert.assertEquals(expected, actual);
+	}
+	
 }
